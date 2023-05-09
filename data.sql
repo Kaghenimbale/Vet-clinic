e932f0c9b77ccec768f8ba938d6d268d8fd3b29b
Kagheni <Kaghenimbale@gmail.com>
2023-05-09 11:31:12 +0200

    Insert values in the animals tables

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(
+name,
+date_of_birth,
+escape_attempts,
+neutered,
+weight_kg
+)
+VALUES ('Agumon', 'Feb 3, 2020', 0, '0', 10.23), ('Gabumon', 'Nov 15, 2018', 2, '1', 8), ('Pikachu', 'Jan 07, 2021', 1, '0', 15.04), ('Devimon', 'May 12, 2017', 5, '1', 11);
