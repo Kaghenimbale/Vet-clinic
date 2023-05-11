abdecedef58f4c298cd61e15eb426f8d850cf4cd
Kagheni <Kaghenimbale@gmail.com>
2023-05-11 07:32:58 +0200

    Add table owner and species

--- schema.sql
+++ schema.sql
@@ -9,3 +9,16 @@ neutered BIT,
 weight_kg DECIMAL,
 species VARCHAR
 );
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR(50),
+age INT,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(50),
+PRIMARY KEY(id)
+);
