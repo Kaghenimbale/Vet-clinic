adc971f0b4dd866c7845f2b4ac8721c80042d883
Kagheni <Kaghenimbale@gmail.com>
2023-05-11 13:45:15 +0200

    Add foreign key syntax

--- schema.sql
+++ schema.sql
@@ -22,3 +22,19 @@ id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(50),
 PRIMARY KEY(id)
 );
+
+ALTER TABLE animals ADD species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals ADD owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners 
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
