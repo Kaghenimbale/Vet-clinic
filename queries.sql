22e046f8fa18fe6aef57b116c48815250c5e0212
Kagheni <Kaghenimbale@gmail.com>
2023-05-11 15:05:04 +0200

    Join TABLE

--- queries.sql
+++ queries.sql
@@ -92,3 +92,30 @@ SELECT O.id, full_name, name FROM owners O
 LEFT JOIN animals
 ON O.id = animals.owner_id;
 
+SELECT S.name, COUNT(*) AS "ANIMALS IN SPECIES"
+FROM animals
+JOIN species S
+ON animals.species_id = S.id
+GROUP BY S.name;
+
+SELECT A.id, full_name, A.name AS "DIGIMON OWNED BY Jennifer Orwell"
+FROM owners O
+JOIN animals A
+ON O.id = A.owner_id
+JOIN species
+ON A.species_id = species.id
+WHERE O.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT A.id, A.name AS "ANIMALS WHO NEVER TRIED ESCAPE", full_name, escape_attempts
+FROM animals A
+JOIN owners
+ON A.owner_id = owners.id
+WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
+
+SELECT full_name, COUNT(A.name) AS "NUMBER OF ANIMALS OWNED"
+FROM animals A
+JOIN owners
+ON A.owner_id = owners.id
+GROUP BY full_name
+ORDER BY "NUMBER OF ANIMALS OWNED" DESC LIMIT 1;
+
